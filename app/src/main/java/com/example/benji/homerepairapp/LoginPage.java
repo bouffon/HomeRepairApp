206462a98963d8ca7dc64dda2952d3c773f886bf
Benjamin Morrison <bmorr038@uottawa.ca>
2018-12-05 16:59:31 -0500

    Added a few more default service providers

--- app/src/main/java/com/example/benji/homerepairapp/LoginPage.java
+++ app/src/main/java/com/example/benji/homerepairapp/LoginPage.java
@@ -33,7 +33,7 @@ public class LoginPage extends AppCompatActivity {
             Homeowner user = new Homeowner("bob", "bob", "bob", "bob", "bob@bob.ca", "9059059055", "bob");
             db.addUser(user);
         }
-        //add default service provider
+        //add default service provider 1
         if (db.findUser("guy", "guy") == null){
             ServiceProvider user = new ServiceProvider("guy", "guy", "guy", "guy", "guy@guy.ca", "9059059055", "69 Admin place");
             db.addUser(user);
@@ -42,16 +42,36 @@ public class LoginPage extends AppCompatActivity {
                     "__ : __","__ : __");
             db.updateRating("guy","guy", 3,"");
         }
-        //add default service
+        //add default service provider 2
+        if (db.findUser("jim", "jim") == null){
+            ServiceProvider user = new ServiceProvider("jim", "jim", "jim", "michael", "jim@jim.ca", "9059059055", "12 jim place");
+            db.addUser(user);
+            db.addSPInfo("jim","jim","defaultCompany","defaultDescription",true,"11 : 00","12 : 00","13 : 00",
+                    "23 : 00","__ : __","__ : __","14 : 00","15 : 30","__ : __","__ : __","__ : __","__ : __",
+                    "__ : __","__ : __");
+            db.updateRating("jim","jim", 2,"");
+        }
+        //add default service provider 3
+        if (db.findUser("kate", "kate") == null){
+            ServiceProvider user = new ServiceProvider("kate", "kate", "kate", "timmins", "kate@kate.ca", "9059059055", "22 kim place");
+            db.addUser(user);
+            db.addSPInfo("kate","kate","defaultCompany","defaultDescription",true,"09 : 00","12 : 00","17 : 00",
+                    "20 : 00","__ : __","__ : __","14 : 00","15 : 30","__ : __","__ : __","__ : __","__ : __",
+                    "06 : 00","12 : 00");
+            db.updateRating("kate","kate", 4,"");
+        }
+        //add default services
         if (db.findService("Roofing") == null) {
             Service s = new Service("Roofing", 25);
             db.addService(s);
             db.addSPService("guy", "guy", s.getServiceName());
+            db.addSPService("jim", "jim", s.getServiceName());
         }
         if (db.findService("Plumbing") == null) {
             Service s = new Service("Plumbing", 29.75);
             db.addService(s);
             db.addSPService("guy", "guy", s.getServiceName());
+            db.addSPService("kate", "kate", s.getServiceName());
         }
         if (db.findService("Appliance Repair") == null) {
             Service s = new Service("Appliance Repair", 35);
@@ -62,6 +82,7 @@ public class LoginPage extends AppCompatActivity {
             Service s = new Service("Landscaping", 21.50);
             db.addService(s);
             db.addSPService("guy", "guy", s.getServiceName());
+            db.addSPService("jim", "jim", s.getServiceName());
         }
         if (db.findService("Gutter Cleaning") == null) {
             Service s = new Service("Gutter Cleaning", 23);
@@ -72,6 +93,7 @@ public class LoginPage extends AppCompatActivity {
             Service s = new Service("Painting", 22.25);
             db.addService(s);
             db.addSPService("guy", "guy", s.getServiceName());
+            db.addSPService("kate", "kate", s.getServiceName());
         }
         if (db.findService("Mold Remediation") == null) {
             Service s = new Service("Mold Remediation", 33.25);
@@ -87,11 +109,13 @@ public class LoginPage extends AppCompatActivity {
             Service s = new Service("Extermination", 24.99);
             db.addService(s);
             db.addSPService("guy", "guy", s.getServiceName());
+            db.addSPService("kate", "kate", s.getServiceName());
         }
         if (db.findService("Electrical Work") == null) {
             Service s = new Service("Electrical Work", 24.99);
             db.addService(s);
             db.addSPService("guy", "guy", s.getServiceName());
+            db.addSPService("jim", "jim", s.getServiceName());
         }
     }
 
